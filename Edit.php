d46836f5214695447eec926f2d1d8862667874c6
ernaromauli <58617840+ernaromauli@users.noreply.github.com>
2019-12-19 18:19:34 +0700

    Update Edit.php

--- Edit.php
+++ Edit.php
@@ -1,77 +1,161 @@
-<?php 
- 
-include_once("conection.php");
-
-if(isset($_POST['edit']))
-{
-$id = $_POST['id'];
-$Nama_Mahasiswa= $_POST['Nama_Mahasiswa'];
-$NIM= $_POST['NIM'];
-$Nama_Prodi = $_POST['Nama_Prodi'];
-$Jurusan = $_POST['Jurusan'];
-$Email = $_POST['Email'];
-
- 
-$result = mysqli_query($mysqli, "UPDATE tbl_admin SET Nama_Mahasiswa ='$Nama_Mahasiswa', NIM ='$NIM', Nama_Prodi='$Nama_Prodi', Email='$Email', Telp='$Telp' WHERE id='$id'");
- 
-header("location:Admin.php?pesan=update");
- 
-?>
 <?php
+include ("conection1.php");
+
+if( !isset($_GET['id']) ){
+    header('Location: Admin.php');    
+}
 
 $id = $_GET['id'];
 
-$result = mysqli_query($mysqli, "SELECT * FROM tbl_admin WHERE id=$id");
+$sql = "SELECT * FROM tbl_admin where NIM=$id";
+$query = mysqli_query($conn, $sql);
+$a = mysqli_fetch_assoc($query);
 
-while($user_data = mysqli_fetch_array($result))
-{
-		echo "<td>".$user_data['Nama_Mahasiswa']."</td>";
-		echo "<td>".$user_data['NIM']."</td>";
-        echo "<td>".$user_data['Nama_Prodi']."</td>";
-        echo "<td>".$user_data['Jurusan']."</td>";
-        echo "<td>".$user_data['Email']."</td>"; 
-	
+if ( mysqli_num_rows($query) < 1 ){
+    die("data tidak ditemukan");
+}
 ?>
-<html>
-<head>  
-    <title>Edit User Data</title>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Data Mahasiswa- Input</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+  <!-- Page level plugin CSS-->
+  <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin.css" rel="stylesheet">
+
 </head>
 
-<body>
-    <a href="Admin.php">Home</a>
-    <br/><br/>
+<body id="page-top">
 
-    <form name="update_user" method="post" action="edit.php">
-        <table border="0">
-            <tr> 
-                <td>Nama Mahasiswa</td>
-                <td><input type="text" name="Nama_Mahasiswa" value=<?php echo $Nama_Mahasiswa;?>></td>
-            </tr>
-			<tr> 
-                <td>NIM</td>
-                <td><input type="text" name="NIM" value=<?php echo $NIM;?>></td>
-            </tr>
-            <tr>  
-                <td>Nama Prodi</td>
-                <td><input type="text" name="Nama_Prodi" value=<?php echo $Nama_Prodi;?>></td>
-            </tr>
-			<tr> 
-                <td>Jurusan</td>
-                <td><input type="text" name="Jurusan" value=<?php echo $Jurusan;?>></td>
-            </tr>
-			<tr>
-                <td>Email</td>
-                <td><input type="text" name="email" value=<?php echo $email;?>></td>
-            </tr>
-            <tr> 
-                <td>Info</td>
-                <td><input type="text" name="info" value=<?php echo $info;?>></td>
-            </tr>
-            <tr>
-                <td><input type="hidden" name="id" value=<?php echo $_GET['id'];?>></td>
-                <td><input type="submit" name="update" value="Update"></td>
-            </tr>
-        </table>
-    </form>
+  <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
+
+    <a class="navbar-brand mr-1" href="Awal.php">Data Mahasiswa</a>
+
+    <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
+      <i class="fas fa-bars"></i>
+    </button>
+
+
+    
+  </nav>
+
+  <div id="wrapper">
+
+    <!-- Sidebar -->
+    <ul class="sidebar navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="Awal.php">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Dashboard</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Admin.php">
+          <i class="fas fa-fw fa-table"></i>
+          <span>Tables</span></a>
+      </li>
+    </ul>
+
+    <div id="content-wrapper">
+
+      <div class="container-fluid">
+
+        <!-- Breadcrumbs-->
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item">
+            <a href="Awal.php">Dashboard</a>
+          </li>
+        </ol>
+
+        <!-- Page Content -->
+        <h1>Update</h1>
+        <hr>
+        <form action="EditProses.php" method="POST">
+        <div class="form-group">
+			<label for="nama">Nama Mahasiswa</label>
+			<input type="text" name="Nama" class="form-control" value="<?php echo $a['Nama_Mahasiswa'] ?>">
+		</div>
+        <div class="form-group">
+			<label for="nama">NIM</label>
+			<input type="text" name="Nim" class="form-control" value="<?php echo $a['NIM'] ?>">
+		</div>
+        <div class="form-group">
+			<label for="nama">Email</label>
+			<input type="text" name="email" class="form-control" value="<?php echo $a['Email'] ?>">
+		</div>
+        <div class="form-group">
+			<label for="nama">ID Matakuliah</label>
+			<input type="text" name="Matakuliah" class="form-control" value="<?php echo $a['ID_Matakuliah'] ?>">
+		</div>
+        <button type="submit" class="btn btn-success btn-sm" name="edit">Submit</button>
+        </form>
+   </div>
+      </div>
+      <!-- /.container-fluid -->
+
+      <!-- Sticky Footer -->
+      <footer class="sticky-footer">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright © Your Website 2019</span>
+          </div>
+        </div>
+      </footer>
+
+    </div>
+    <!-- /.content-wrapper -->
+
+  </div>
+  <!-- /#wrapper -->
+
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+  <!-- Logout Modal-->
+  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+        <div class="modal-footer">
+          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+          <a class="btn btn-primary" href="login.html">Logout</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="js/sb-admin.min.js"></script>
+
 </body>
+
 </html>
